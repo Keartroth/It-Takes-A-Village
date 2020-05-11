ddb52601c55f44ca8a34a824a38445844dd96db2
Keartroth <mecarrolljr@gmail.com>
2020-05-11 10:15:08 -0500

    refactored message dashboard module to reset to the message board when switching villages, rather than attempting to stay in a thread that does not exist for the new village.

--- src/components/message/MessageDashboard.js
+++ src/components/message/MessageDashboard.js
@@ -44,15 +44,19 @@ export const MessagDashboard = (props) => {
         This effect hook determines which list is shown
         based on the state of the `activeList` variable.
     */
-    useEffect(() => {
-        if (activeList === "messageBoard") {
-            setComponents(showMessageBoard)
-        }
-        else if (activeList === "messageBoardThread") {
-            setComponents(showMessageBoardThread)
-        }
-    }, [activeList, villageId])
+   useEffect(() => {
+       if (activeList === "messageBoard") {
+           setComponents(showMessageBoard)
+       }
+       else if (activeList === "messageBoardThread") {
+           setComponents(showMessageBoardThread)
+       }
+   }, [activeList, villageId])
 
+   useEffect(() => {
+    setActiveList("messageBoard")
+   }, [villageId])
+   
     return (
         <section className="messageBoardMasterContainer">
             <div className="listDisplay">
